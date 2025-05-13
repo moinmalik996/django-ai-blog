31a10046e0358ab8c8766fca66dcff8bdcb99451
Moin Abbas <moin.malik996@gmail.com>
2025-05-14 00:00:17 +0100

    gitignore added

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -171,3 +171,6 @@ EMAIL_HOST_PASSWORD = 'hfqjknkonjknovjknkjljglydkbkjjej'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+NPM_BIN_PATH = '/usr/bin/npm'
