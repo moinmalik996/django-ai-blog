ead7d5e63e3e917d1282b5d503804b0f1392dcf7
Moin Abbas <moin.malik996@gmail.com>
2025-05-14 23:16:24 +0100

    project structure updated

--- blogApp/wsgi.py
+++ blogApp/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+from decouple import config
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('DJANGO_SETTINGS_MODULE'))
 
 application = get_wsgi_application()
